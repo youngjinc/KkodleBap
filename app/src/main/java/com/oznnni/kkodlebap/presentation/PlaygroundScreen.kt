5cb5c68be7acc588602e0212d2eb86a7779a03f3
youngjinc <cyjin6789@gmail.com>
2025-04-10 23:23:48 +0900

    🎨️ 메인 화면 UI 구현

--- app/src/main/java/com/oznnni/kkodlebap/presentation/PlaygroundScreen.kt
+++ app/src/main/java/com/oznnni/kkodlebap/presentation/PlaygroundScreen.kt
@@ -1,7 +1,39 @@
 package com.oznnni.kkodlebap.presentation
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import com.oznnni.kkodlebap.R
+import com.oznnni.kkodlebap.ui.theme.KkodlebapTheme
+import com.oznnni.kkodlebap.ui.theme.Typography
 
 @Composable
 fun PlaygroundScreen() {
@@ -10,7 +42,145 @@ fun PlaygroundScreen() {
 
 @Composable
 fun PlaygroundContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(KkodlebapTheme.colors.white),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 18.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(width = 65.dp, height = 50.dp)
+                    .align(Alignment.Center),
+                painter = painterResource(id = R.drawable.ic_kkodlebap_logo),
+                contentDescription = "꼬들밥 로고"
+            )
 
+            Box(modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 6.dp)
+                .clip(CircleShape)
+                .clickable { }
+                .size(48.dp), contentAlignment = Alignment.Center) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_question_mark),
+                    contentDescription = "도움말",
+                    tint = Color.Unspecified
+                )
+            }
+        }
+
+        LazyVerticalGrid(
+            modifier = Modifier
+                .padding(top = 56.dp)
+                .fillMaxWidth(0.75f),
+            columns = GridCells.Fixed(6),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+        ) {
+            items(36) {
+                Cell(
+                    modifier = Modifier
+                        .weight(1f)
+                        .aspectRatio(1f), jamo = "ㅈ"
+                )
+            }
+        }
+
+        Text(
+            modifier = Modifier.padding(top = 24.dp, bottom = 5.dp),
+            text = "자모 6개를 입력해 주세요!",
+            style = Typography.SuitR5,
+            color = KkodlebapTheme.colors.red700
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+                listOf("ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ").fastForEach {
+                    Key(key = it)
+                }
+            }
+
+            Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+                listOf("ㅁ", "ㄴ", "ㅇ", "ㄹ", "ㅎ", "ㅗ", "ㅓ", "ㅏ", "ㅣ").fastForEach {
+                    Key(key = it)
+                }
+            }
+
+            Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+                Key(iconRes = R.drawable.ic_backspace, backgroundColor = KkodlebapTheme.colors.gray200, description = "지우기 아이콘")
+
+                listOf("ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ").fastForEach {
+                    Key(key = it)
+                }
+
+                Key(iconRes = R.drawable.ic_check, backgroundColor = KkodlebapTheme.colors.blue600, description = "정답 제출 체크 아이콘")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(72.dp))
+    }
+}
+
+@Composable
+private fun Cell(modifier: Modifier = Modifier, jamo: String) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(KkodlebapTheme.colors.blue100), contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = jamo,
+            style = Typography.SuitM1,
+            color = KkodlebapTheme.colors.gray700
+        )
+    }
+}
+
+@Composable
+private fun Key(modifier: Modifier = Modifier, key: String) {
+    Box(
+        modifier = modifier
+            .size(width = 33.dp, height = 41.dp)
+            .clip(RoundedCornerShape(6.dp))
+            .clickable { }
+            .background(KkodlebapTheme.colors.blue100),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = key,
+            style = Typography.SuitM3,
+            color = KkodlebapTheme.colors.gray700
+        )
+    }
+}
+
+@Composable
+private fun Key(modifier: Modifier = Modifier, @DrawableRes iconRes: Int, backgroundColor: Color, description: String) {
+    Box(
+        modifier = modifier
+            .size(width = 33.dp, height = 41.dp)
+            .clip(RoundedCornerShape(6.dp))
+            .clickable { }
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            tint = Color.Unspecified,
+            contentDescription = description
+        )
+    }
 }
 
 @Preview(showBackground = true)
