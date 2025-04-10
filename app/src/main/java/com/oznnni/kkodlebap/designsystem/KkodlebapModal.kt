420ec2613546ccea4f25bb97ead8ca41b8c277b5
youngjinc <cyjin6789@gmail.com>
2025-04-10 23:24:26 +0900

    🎨️ 모달 UI 추가

---
+++ app/src/main/java/com/oznnni/kkodlebap/designsystem/KkodlebapModal.kt
@@ -0,0 +1,42 @@
+package com.oznnni.kkodlebap.designsystem
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetDefaults
+import androidx.compose.material3.ModalBottomSheetProperties
+import androidx.compose.material3.SheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.oznnni.kkodlebap.ui.theme.KkodlebapTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KkodlebapModal(
+    isOpen: Boolean,
+    sheetState: SheetState,
+    properties: ModalBottomSheetProperties = ModalBottomSheetDefaults.properties,
+    onDismissRequest: () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    if (!isOpen) return
+    ModalBottomSheet(
+        sheetState = sheetState,
+        shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+        onDismissRequest = onDismissRequest,
+        scrimColor = Color.Black.copy(alpha = 0.6f),
+        containerColor = KkodlebapTheme.colors.white,
+        properties = properties,
+        dragHandle = null,
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            content()
+        }
+    }
+}
