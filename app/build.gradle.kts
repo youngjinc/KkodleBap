92b3e48622ba5fcf7ddc8d2e1d9fd307ca615116
youngjinc <cyjin6789@gmail.com>
2025-04-19 00:09:56 +0900

    🙈 버전 코드 2 -> 4 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.oznnni.kkodlebap"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
